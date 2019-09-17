2b873921ffd2b181a60140a50e0acb167f388fc4
AbuSufianIslam <thebooming1992@gmail.com>
2019-09-17 15:34:43 -0400

    Done

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-const express = require("express");
-const morgan = require("morgan");
-const client = require("./db");
-const postList = require("./views/postList");
-const postDetails = require("./views/postDetails");
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/posts');
+// const client = require('./db');
+// const postList = require('./views/postList');
+// const postDetails = require('./views/postDetails');
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const baseQuery = "SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n";
+//app.use(express.urlencoded({ extended: true }));
+app.use('/posts', routes);
 
-app.get("/", async (req, res, next) => {
-  try {
-    const data = await client.query(baseQuery);
-    res.send(postList(data.rows));
-  } catch (error) { next(error) }
+app.get('/', (req, res) => {
+	res.redirect('/posts');
 });
 
-app.get("/posts/:id", async (req, res, next) => {
-  try {
-    const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
-    const post = data.rows[0];
-    res.send(postDetails(post));
-  } catch (error) { next(error) }
-});
+// app.get("/", async (req, res, next) => {
+//   try {
+//     const data = await client.query(baseQuery);
+//     res.send(postList(data.rows));
+//   } catch (error) { next(error) }
+// });
+
+// app.get("/posts/:id", async (req, res, next) => {
+//   try {
+//     const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
+//     const post = data.rows[0];
+//     res.send(postDetails(post));
+//   } catch (error) { next(error) }
+// });
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+	console.log(`App listening in port ${PORT}`);
 });
